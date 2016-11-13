15209f418fb523585ae696f4fdcd65d005e6d6ce
Brandon R Barnett <bbarnett@rightpoint.com>
2016-11-13 11:24:50 -0600

    Domain models

--- BB.OrderFulfillment.Domain/Models/Order.cs
+++ BB.OrderFulfillment.Domain/Models/Order.cs
@@ -1,6 +1,37 @@
-﻿namespace BB.OrderFulfillment.Domain.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BB.OrderFulfillment.Domain.Models
 {
     public class Order
     {
+        public string Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerAddress { get; set; }
+
+        public IEnumerable<Sku> Skus { get; set; }
+
+        public Order() { }
+
+        public Order(string id, string customerName, string customerAddress, ICollection<Sku> skus)
+        {
+            if(String.IsNullOrEmpty(id)) throw new ArgumentNullException(nameof(id));
+            if(String.IsNullOrEmpty(customerName)) throw new ArgumentNullException(nameof(customerName));
+            if(String.IsNullOrEmpty(customerAddress)) throw new ArgumentNullException(nameof(customerAddress));
+            if(!skus.Any()) throw new ArgumentException("Order must contain sold SKUs", nameof(skus));
+
+            this.Id = id;
+            this.CustomerName = customerName;
+            this.CustomerAddress = customerAddress;
+            this.Skus = skus;
+        }
+
+        public decimal GetOrderTotal()
+        {
+            return this.Skus.Sum(_ => _.GetTotalCost());
+        }
     }
 }
---
+++ BB.OrderFulfillment.Domain/Models/Sku.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BB.OrderFulfillment.Domain.Models
+{
+    public class Sku
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Sku() { }
+
+        public Sku(string name, decimal price, int quantity)
+        {
+            if(String.IsNullOrEmpty(name)) throw new ArgumentNullException(nameof(name));
+            if(price < 0) throw new ArgumentOutOfRangeException(nameof(price));
+            if(quantity < 0) throw new ArgumentNullException(nameof(quantity));
+
+            this.Name = name;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public decimal GetTotalCost()
+        {
+            return this.Price * this.Quantity;
+        }
+    }
+}
