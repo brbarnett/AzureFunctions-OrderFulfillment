dab100670c54a6495886eb20ee6bdd1ef16a7d61
Brandon R Barnett <bbarnett@rightpoint.com>
2016-11-13 12:46:37 -0600

    Adding OrderEntity for storage (table)

---
+++ BB.OrderFulfillment.Orders/StorageModels/OrderEntity.cs
@@ -0,0 +1,13 @@
+﻿using BB.OrderFulfillment.Domain.Models;
+using BB.OrderFulfillment.Orders.ExternalModels;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace BB.OrderFulfillment.Orders.StorageModels
+{
+    public class OrderEntity : TableEntity
+    {
+        public ECommerceOrder ExternalOrder { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
